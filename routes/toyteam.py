664dc6b1a8ef1096af0f3032103590d5563e3951
entangelk <kdtyohan@gmail.com>
2024-01-05 16:49:03 +0900

    update

--- routes/toyteam.py
+++ routes/toyteam.py
@@ -12,18 +12,18 @@ templates = Jinja2Templates(directory="templates/")
 # 응시한 애들 값
 @router.get("/data_list", response_class=HTMLResponse) # 펑션 호출 방식
 async def forms(request:Request):
-    return templates.TemplateResponse(name="positionings/forms.html", context={'request':request})
+    return templates.TemplateResponse(name="toyteam/data_list.html", context={'request':request})
 
 @router.post("/data_list", response_class=HTMLResponse) # 펑션 호출 방식
 async def forms(request:Request):
-    return templates.TemplateResponse(name="positionings/forms.html", context={'request':request})
+    return templates.TemplateResponse(name="toyteam/data_list.html", context={'request':request})
 
 
 # 문제 페이지
 @router.get("/exam_test", response_class=HTMLResponse) # 펑션 호출 방식
 async def forms(request:Request):
-    return templates.TemplateResponse(name="positionings/forms.html", context={'request':request})
+    return templates.TemplateResponse(name="toyteam/exam_test.html", context={'request':request})
 
 @router.post("/exam_test", response_class=HTMLResponse) # 펑션 호출 방식
 async def forms(request:Request):
-    return templates.TemplateResponse(name="positionings/forms.html", context={'request':request})
+    return templates.TemplateResponse(name="toyteam/exam_test.html", context={'request':request})
